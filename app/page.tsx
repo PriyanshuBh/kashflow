aa73f6d4b78328e4d021740bb8502abe1a76cb51
PriyanshuBh <priyanshubh2003@gmail.com>
2024-07-06 12:26:20 +0530

    addhome page

---
+++ app/page.tsx
@@ -0,0 +1,310 @@
+
+import Link from "next/link";
+import Footer from "@/components/footer";
+import SectionDivider from "@/components/sectiondivider";
+export default async function Home() {
+  // const session = await auth();
+  // const user = session?.user as UserExt;
+  // const categories = (await prisma.category.findMany()) as Category[];
+
+  // if (!user) redirect("/auth/login");
+  // if (!user.settings && _.isEmpty(user.budgets)) redirect("/wizard");
+
+  // const budgets = await prisma.budget.findMany({
+  //   where: {
+  //     userId: user.id,
+  //   },
+  // });
+
+  return (
+    <main className="bg-background w-full">
+    <div>
+      <div className="relative overflow-hidden ">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 md:pt-36 pt-24 pb-10">
+          <div className="flex justify-center">
+            <a
+              className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200"
+              href="https://priyanshubharti.vercel.app"
+              target="_blank"
+            >
+              Priyanshu Bharti- Portfolio
+              <span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </a>
+          </div>
+
+          <div className="mt-12 max-w-2xl text-center mx-auto ">
+            <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200 !leading-tight">
+            Budget Tracker -
+              <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent !leading-tight">
+                {" "}
+                Manage Your Finances with Ease
+              </span>
+            </h1>
+          </div>
+
+          <div className="mt-8 max-w-3xl text-center mx-auto">
+            <p className="text-lg text-gray-600 dark:text-neutral-400">
+            Budget Tracker is a simple and intuitive web app that helps you stay on top of your finances. Easily track your income and expenses, add new transactions, and get a clear view of your monthly spending habits — all in one place.
+            </p>
+          </div>
+
+          <div className="mt-16 gap-3 flex justify-center">
+            
+              <Link
+                className="inline-flex justify-center items-center 
+      gap-x-3 text-center bg-gradient-to-tl from-blue-600
+       to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
+                href="/dashboard"
+              >
+                Get started
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </Link>
+            
+          </div>
+          <div className=" item-center flex justify-center">
+             <SectionDivider/>
+          </div>
+         
+        </div>
+      </div>
+
+    
+
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-10 mx-auto">
+        <div className=" text-center w-full mb-28 ">
+        <span className="bg-clip-text text-4xl md:text-5xl lg:text-6xl text-center font-bold bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent !leading-tight">
+             Features
+           </span>
+        </div>
+    
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-2">
+          <a
+            className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
+            href="#"
+          >
+            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
+              <svg
+                className="flex-shrink-0 size-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <rect width="10" height="14" x="3" y="8" rx="2" />
+                <path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4" />
+                <path d="M8 18h.01" />
+              </svg>
+            </div>
+            <div className="mt-5">
+              <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
+              Categorized Transactions
+              </h3>
+              <p className="mt-1 text-gray-600 dark:text-neutral-400">
+              Organize your income and expenses into categories like Food, Rent, Salary, Shopping, etc.,
+              </p>
+              <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline font-medium">
+                Learn more
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </div>
+          </a>
+
+          <a
+            className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
+            href="#"
+          >
+            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
+              <svg
+                className="flex-shrink-0 size-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M20 7h-9" />
+                <path d="M14 17H5" />
+                <circle cx="17" cy="17" r="3" />
+                <circle cx="7" cy="7" r="3" />
+              </svg>
+            </div>
+            <div className="mt-5">
+              <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
+              Real-Time Balance Calculation
+              </h3>
+              <p className="mt-1 text-gray-600 dark:text-neutral-400">
+              Instantly see your current balance calculated from total income and expenses.
+              </p>
+              <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline font-medium">
+                Learn more
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </div>
+          </a>
+
+          <a
+            className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
+            href="#"
+          >
+            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
+              <svg
+                className="flex-shrink-0 size-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+              </svg>
+            </div>
+            <div className="mt-5">
+              <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
+              Visual Reports 
+              </h3>
+              <p className="mt-1 text-gray-600 dark:text-neutral-400">
+              Display charts (e.g., pie chart or bar graph) showing how your money is being spent.
+              </p>
+              <p>.</p>
+              <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline font-medium">
+                Learn more
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </div>
+          </a>
+
+          <a
+            className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800"
+            href="#"
+          >
+            <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
+              <svg
+                className="flex-shrink-0 size-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
+                <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
+              </svg>
+            </div>
+            <div className="mt-5">
+              <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">
+              Monthly/Weekly Tracking
+              </h3>
+              <p className="mt-1 text-gray-600 dark:text-neutral-400">
+              View your financial activity filtered by date range to understand short- and long-term trends.
+              </p>
+              <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline font-medium">
+                Learn more
+                <svg
+                  className="flex-shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <Footer/>
+    </main>
+  );
+}
