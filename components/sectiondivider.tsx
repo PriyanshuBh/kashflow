48bfd48c1f4b073fa67a378453b5211c7e40f8de
PriyanshuBh <priyanshubh2003@gmail.com>
2024-07-31 12:38:48 +0530

    add section devider

---
+++ components/sectiondivider.tsx
@@ -0,0 +1,11 @@
+
+export default function sectionDivider() {
+  return (
+    <div>
+         <div
+      className="bg-gray-400 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20 relative z-11"
+      
+    ></div>
+    </div>
+  )
+}
